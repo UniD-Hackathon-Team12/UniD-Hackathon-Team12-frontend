f0a966a764e56b9150d8a8c6f66f6290b0489117
bwj2800 <bwj2800@naver.com>
2022-11-05 17:49:38 +0900

    DELETE login, signup, create props

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Main from "./pages/Main";
+import NovelDetail from "./pages/NovelDetail";
+import MyPage from "./pages/MyPage";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Create from "./pages/Create";
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
+      {/* <Main/> */}
+      <Create/>
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Button, TextField, Stack,Container, Typography, Box } from '@mui/material';
 
-type LoginProps = {
-  name: string;
-};
 
-const Login: React.FC<LoginProps> = ({ name }) => (
-    <div>
-        {/* <div>Hello, {name}</div> */}
-        <Container fixed maxWidth="lg">
-            <Stack alignItems="center" spacing={3}>
-                <TextField id="outlined-basic" sx={{width:"50%"}} label="ID" variant="outlined" />
-                <TextField id="outlined-basic" sx={{width:"50%"}} label="PASSWORD" variant="outlined" />
-                <Button sx={{width:"30%"}} variant="contained">Login</Button>
-                <Typography color="primary">계정이 없으신가요?</Typography>
-            </Stack>
-        </Container>
-    </div>
-);
-
+function Login() {
+    return (
+        <div>
+            {/* <div>Hello, {name}</div> */}
+            <Container fixed maxWidth="lg">
+                <Stack alignItems="center" spacing={3}>
+                    <TextField id="outlined-basic" sx={{width:"50%"}} label="ID" variant="outlined" />
+                    <TextField id="outlined-basic" sx={{width:"50%"}} label="PASSWORD" variant="outlined" />
+                    <Button sx={{width:"30%"}} variant="contained">Login</Button>
+                    <Typography color="primary">계정이 없으신가요?</Typography>
+                </Stack>
+            </Container>
+        </div>
+    );
+  }
+  
 export default Login;
