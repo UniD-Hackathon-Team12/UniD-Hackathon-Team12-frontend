0aee11b1564eae06c2f20ee70adbb0e4585b65e8
NormalPlayerJSH <jsh010904@gmail.com>
2022-11-05 22:55:09 +0900

    asdf

--- src/Routers.tsx
+++ src/Routers.tsx
@@ -1,23 +1,29 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import Login from './pages/Login';
-import Main from './pages/Main';
-import MyPage from './pages/MyPage';
-import NovelDetail from './pages/NovelDetail';
-import Signup from './pages/Signup';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Login from "./pages/Login";
+import Main from "./pages/Main";
+import MyPage from "./pages/MyPage";
+import NovelDetail from "./pages/NovelDetail";
+import Signup from "./pages/Signup";
 const Routers = () => {
-    return (
-        <BrowserRouter>
-            <NavBar />
-            <Routes>
-                <Route path='/' element={<Main />} />
-                <Route path='/novel-detail' element={<NovelDetail />} />
-                <Route path='/mypage' element={<MyPage />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/signup' element={<Signup />} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Main type="main" />} />
+        <Route path="/search/:word" element={<Main type="search" />} />
+        <Route path="/keyword" element={<Main type="keyword" />} />
+        <Route
+          path="/keyword/search/:word"
+          element={<Main type="keywordSearch" />}
+        />
+        <Route path="/novel-detail" element={<NovelDetail />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 export default Routers;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,12 +7,44 @@ function NavBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar sx={{ height: 100, background: "#E0BFE6", justifyContent: "space-between" }}>
-          <img height='90' src="logo.png" />
+        <Toolbar
+          sx={{
+            height: 100,
+            background: "#E0BFE6",
+            justifyContent: "space-between",
+          }}
+        >
+          <Link to="/">
+            <img height="90" src="/logo.png" />
+          </Link>
           <Box>
-
-            <Link to='/login'><Button size="large" style={{ marginRight: "10px", background: "white", border: "solid", borderRadius: 10, color: "black" }}>로그인</Button></Link>
-            <Link to='/Signup'><Button size="large" style={{ background: "white", border: "solid", borderRadius: 10, color: "black" }}>회원가입</Button></Link>
+            <Link to="/login">
+              <Button
+                size="large"
+                style={{
+                  marginRight: "10px",
+                  background: "white",
+                  border: "solid",
+                  borderRadius: 10,
+                  color: "black",
+                }}
+              >
+                로그인
+              </Button>
+            </Link>
+            <Link to="/Signup">
+              <Button
+                size="large"
+                style={{
+                  background: "white",
+                  border: "solid",
+                  borderRadius: 10,
+                  color: "black",
+                }}
+              >
+                회원가입
+              </Button>
+            </Link>
           </Box>
         </Toolbar>
       </AppBar>
