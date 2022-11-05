290d3a944517d7eb6b0de83f59ac55d38fc96784
bwj2800 <bwj2800@naver.com>
2022-11-05 19:56:55 +0900

    FEAT alignment

--- src/pages/Create.tsx
+++ src/pages/Create.tsx
@@ -6,47 +6,47 @@ function Create() {
     return (
         <div>
             {/* <div>Hello, {name}</div> */}
-            <Container fixed maxWidth="lg">
-                <Grid container alignItems="center" spacing={5}>
-                    <Grid item xs={2}>
+            <Container fixed maxWidth="lg" sx={{marginTop:"50px"}}>
+                <Grid container alignItems="flex-start" spacing={5}>
+                    <Grid item xs={2} container direction="row" justifyContent="flex-end">
                         <Typography>카테고리</Typography>
                     </Grid>
-                    <Grid item xs={10}>
+                    <Grid item xs={10} container direction="row" justifyContent="flex-start">
                         <Typography>한글자</Typography>
                     </Grid>
 
-                    <Grid item xs={2}>
+                    <Grid item xs={2} container direction="row" justifyContent="flex-end">
                         <Typography>키워드</Typography>
                     </Grid>
-                    <Grid item xs={10}>
+                    <Grid item xs={10} container direction="row" justifyContent="flex-start">
                         <TextField id="outlined-basic" sx={{width:"100%"}} variant="outlined" />
                         <Icon>add_circle_outline</Icon>
                     </Grid>
 
-                    <Grid item xs={2}>
+                    <Grid item xs={2} container direction="row" justifyContent="flex-end">
                     <Typography>최대 릴레이 개수</Typography>
                     </Grid>
-                    <Grid item xs={10}>
-                    <TextField
-                        id="outlined-number"
-                        InputProps={{
-                            endAdornment: <InputAdornment position="start">개</InputAdornment>,
-                        }}
-                        type="number"
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                        />
+                    <Grid item xs={10} container direction="row" justifyContent="flex-start">
+                        <TextField
+                            id="outlined-number"
+                            InputProps={{
+                                endAdornment: <InputAdornment position="start">개</InputAdornment>,
+                            }}
+                            type="number"
+                            InputLabelProps={{
+                                shrink: true,
+                            }}
+                            />
                     </Grid>
 
-                    <Grid item xs={2}>
+                    <Grid item xs={2} container direction="row" justifyContent="flex-end">
                     <Typography>시작 스토리</Typography>
                     </Grid>
-                    <Grid item xs={10}>
-                        <TextField id="outlined-basic" sx={{width:"100%"}} variant="outlined" />
+                    <Grid item xs={10} container direction="row" justifyContent="flex-start">
+                    <TextField sx={{width:"100%"}} id="outlined-multiline-static" multiline rows={4}/>
                     </Grid>
                 </Grid>
-                <Button sx={{width:"30%", marginTop:"30px"}} variant="contained">등록</Button>
+                <Button sx={{background:"#E0BFE6", width:"30%", marginTop:"30px"}} variant="contained">등록</Button>
             </Container>
         </div>
     );
