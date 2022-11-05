9f78dd93c2f545c3032a664cca9bb988cb416fc6
bwj2800 <bwj2800@naver.com>
2022-11-05 17:25:04 +0900

    ADD login, signup, create page

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { Button, TextField, Stack,Container, Typography, Box } from '@mui/material';
 
-type LoginProps = {
+type SignupProps = {
   name: string;
 };
 
-const Login: React.FC<LoginProps> = ({ name }) => (
+const Signup: React.FC<SignupProps> = ({ name }) => (
     <div>
         {/* <div>Hello, {name}</div> */}
         <Container fixed maxWidth="lg">
             <Stack alignItems="center" spacing={3}>
                 <TextField id="outlined-basic" sx={{width:"50%"}} label="ID" variant="outlined" />
                 <TextField id="outlined-basic" sx={{width:"50%"}} label="PASSWORD" variant="outlined" />
-                <Button sx={{width:"30%"}} variant="contained">Login</Button>
-                <Typography color="primary">계정이 없으신가요?</Typography>
+                <Button sx={{width:"30%", marginTop:"30px"}} variant="contained">Sign Up</Button>
             </Stack>
         </Container>
     </div>
 );
 
-export default Login;
+export default Signup;
