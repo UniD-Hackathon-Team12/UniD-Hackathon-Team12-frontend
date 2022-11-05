ee7038543e76191f2f7eb5bb6e2ebb7d11f71c7f
NormalPlayerJSH <jsh010904@gmail.com>
2022-11-05 18:01:54 +0900

    noveldetail

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Main from "./pages/Main";
+import NovelDetail from "./pages/NovelDetail";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <NovelDetail />
     </div>
   );
 }
--- src/pages/NovelDetail.tsx
+++ src/pages/NovelDetail.tsx
@@ -1,22 +1,63 @@
-
-import { Grid, TextField } from "@mui/material";
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Box } from "@mui/system";
 import React from "react";
+import NovelCard from "../components/NovelCard";
 import NovelDetailItem from "../components/NovelDetailItem";
 
 function NovelDetail() {
+  return (
     <div>
-        <div>
-            <Grid container spacing={2} paddingX={3} marginTop={3}>
-                {Array(10)
-                    .fill(0)
-                    .map((_, i) => (
-                        <NovelDetailItem
-                            storyItem={`첫번재 댓글`}
-                        />
-                    ))}
-            </Grid>
-        </div>
-        <TextField fullWidth label="fullWidth" id="fullWidth" />
+      <Grid container spacing={2} paddingX={3} marginY={3}>
+        <NovelCard
+          title={`첫번째 문장입니다 123123`}
+          keyword={["키워드1", "키워드2", "키워드3"]}
+          type={"한 문장 (30자)"}
+          maxCount={300}
+          currentCount={30}
+          like={100}
+          forDetail
+        />
+        {Array(10)
+          .fill(0)
+          .map((_, i) => (
+            <NovelDetailItem
+              maker={`maker${i}`}
+              content={
+                "가나다라마바사 아자차카타파하. 가나다라마바사 아자차카타파하. 가나다라마바사 아자차카타파하. 가나다라마바사 아자차카타파하. 가나다라마바사 아자차카타파하. 가나다라마바사 아자차카타파하. 가나다라마바사 아자차카타파하. 가나다라마바사 아자차카타파하."
+              }
+              number={i + 1}
+            />
+          ))}
+        <Grid item xs={12}>
+          <Button variant="contained">더 보기</Button>
+        </Grid>
+        <Grid item xs={12}>
+          <Card>
+            <CardContent>
+              <Box display={"flex"} justifyContent="flex-start" mb={2}>
+                <Typography color="text.secondary">#{11}이 될 예정</Typography>
+              </Box>
+              <TextField
+                label={"작성하기"}
+                fullWidth
+                multiline
+                rows={4}
+              ></TextField>
+              <Box display={"flex"} justifyContent="flex-end" mt={2}>
+                <Button variant="contained">작성</Button>
+              </Box>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </div>
+  );
 }
 export default NovelDetail;
