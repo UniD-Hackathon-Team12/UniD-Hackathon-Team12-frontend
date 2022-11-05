2123728d1021c6fa00f359f8fca3e1e31797f2fc
NormalPlayerJSH <jsh010904@gmail.com>
2022-11-05 17:23:17 +0900

    noveldetail

---
+++ src/pages/NovelDetail.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+function NovelDetail() {
+  return (
+    <Box paddingX={3} my={3} gap={3} display="flex" flexDirection={"column"}>
+      asdf
+    </Box>
+  );
+}
+
+export default NovelDetail;
